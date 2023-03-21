436163f1138ec5bcdb17db566d988f189ca02158
pbutterworth <3683210+pbutterworth@users.noreply.github.com>
2023-03-21 22:38:07 +1300

    fixed import (was referencing local file)

--- custom_components/astralpool_chlorinator/models.py
+++ custom_components/astralpool_chlorinator/models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from .viron_chlorinator.chlorinator import ChlorinatorAPI
+from pychlorinator.chlorinator import ChlorinatorAPI
 from .coordinator import ChlorinatorDataUpdateCoordinator
 
 
