a8e5beee213853ba37c1d899de9c0142d1fef5d4
pbutterworth <3683210+pbutterworth@users.noreply.github.com>
2023-04-04 09:39:21 +1200

    cleaned up code

--- custom_components/astralpool_chlorinator/__init__.py
+++ custom_components/astralpool_chlorinator/__init__.py
@@ -28,14 +28,6 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Chlorinator from a config entry."""
 
-    # coordinator = ChlorinatorDataUpdateCoordinator(hass, chlorinator)
-
-    # hass.data.setdefault(DOMAIN, {})[entry.entry_id] = ChlorinatorData(
-    #     entry.title, chlorinator, coordinator
-    # )
-
-    # hass.data[DOMAIN][entry.entry_id] = entry.data
-
     address: str = entry.data[CONF_ADDRESS]
     accesscode: str = entry.data[CONF_ACCESS_TOKEN]
     ble_device = bluetooth.async_ble_device_from_address(
@@ -58,10 +50,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     )
     return True
-
-
-async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
